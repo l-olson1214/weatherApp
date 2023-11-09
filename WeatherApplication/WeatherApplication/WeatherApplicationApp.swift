6063a3c3de199ebcd1fc891cf7163d7704c690e2
Grey Olson <l.olsonswift@gmail>
2023-11-09 13:49:10 -0500

    Initial Commit

---
+++ WeatherApplication/WeatherApplication/WeatherApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApplicationApp.swift
+//  WeatherApplication
+//
+//  Created by Grey Olson on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
